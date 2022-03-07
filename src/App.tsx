a02d7393c6f4749db447f419da728335fde8a714
rpire <rubenpire7@gmail.com>
2022-03-07 19:28:32 -0400

    Set up application and linters

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <h1>React + TypeScript</h1>
+    <p>This project is created for me to practice the React.ts approach to build applications.</p>
+  </>
+);
 
 export default App;
