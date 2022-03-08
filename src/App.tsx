63fc2d8dfe2c40a2d95e3ab9bd6f7662ac54fef6
rpire <rubenpire7@gmail.com>
2022-03-08 03:09:58 -0400

    Introduce test error

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 
-const App = () => (
-  <>
-    <h1>React + TypeScript</h1>
-    <p>This project is created for me to practice the React.ts approach to build applications.</p>
-  </>
-);
+const App = () => {
+  return (
+    <>
+      <h1>React + TypeScript</h1>
+      <p>This project is created for me to practice the React.ts approach to build applications.</p>
+    </>
+  );
+} 
 
 export default App;
