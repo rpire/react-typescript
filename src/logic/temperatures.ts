41492168097c26c5c2593664ae784f3695136ad1
rpire <rubenpire7@gmail.com>
2022-03-09 01:54:59 -0400

    Refactor conversion logic in temperatures.js

--- src/logic/temperatures.ts
+++ src/logic/temperatures.ts
@@ -1,25 +1,47 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 
-export const initialTemperatures = {
+type State = {
+  celsius: string;
+  farenheit: string;
+}
+
+type Hook = {
+  temperatures: State;
+  convertTemperatures: (event: ChangeEvent) => void;
+}
+
+const initialTemperatures: State = {
   celsius: '',
   farenheit: '',
 };
 
-const fixTemp = (temperature: number): string => temperature.toFixed(2);
+const useTemperatures = (initialState: State = initialTemperatures): Hook => {
+  const [temperatures, setTemperatures] = useState(initialState);
 
-export const calculateTemperatures = (e: ChangeEvent, setTemperatures: CallableFunction): void => {
-  const target = e.target as HTMLInputElement;
-  const temperature = Number(target.value);
+  const fixTemp = (temperature: string, unit: string): string => {
+    if (unit === 'CELS') {
+      return (Number(temperature) * 1.8 + 32).toFixed(2);
+    }
+    return ((Number(temperature) - 32) / 1.8).toFixed(2);
+  };
 
-  if (target.name === 'celsius') {
-    setTemperatures({
-      celsius: temperature.toString(),
-      farenheit: fixTemp(temperature * 1.8 + 32),
-    });
-  } else {
-    setTemperatures({
-      celsius: fixTemp((temperature - 32) / 1.8),
-      farenheit: temperature.toString(),
-    });
-  }
+  const convertTemperatures = (event: ChangeEvent): void => {
+    const target = event.target as HTMLInputElement;
+    const temperature = target.value;
+
+    if (target.name === 'celsius') {
+      setTemperatures({
+        celsius: temperature,
+        farenheit: fixTemp(temperature, 'CELS'),
+      });
+    } else {
+      setTemperatures({
+        celsius: fixTemp(temperature, 'FARE'),
+        farenheit: temperature,
+      });
+    }
+  };
+  return { temperatures, convertTemperatures };
 };
+
+export default useTemperatures;
